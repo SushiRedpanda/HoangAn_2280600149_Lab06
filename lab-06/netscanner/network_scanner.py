7e6739dc9cc9a89fd16a32fee12a228735887ae6
SushiRedpanda <hoangan27804@gmail.com>
2025-04-01 09:21:46 +0700

    [add & fix] lab-06 port scanner and fix ip in network scanner

--- lab-06/netscanner/network_scanner.py
+++ lab-06/netscanner/network_scanner.py
@@ -29,7 +29,7 @@ def get_vendor_by_mac(mac):
         return "Unknow"
     
 def main():
-    ip_range = "192.168.88.103"
+    ip_range = "192.168.88.1/24"
     devices = local_network_scan(ip_range)
     
     print("Devices on the local network:")
